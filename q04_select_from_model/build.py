e94f2c23c728ca1d5d6771189e21ec914320a6f4
cchopade <cchopade@gmail.com>
2018-02-26 04:11:09 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -6,5 +6,15 @@ import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
+def select_from_model(df):
+    import numpy as np
 
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    rfc = RandomForestClassifier(random_state = 9)
+    sfm = SelectFromModel(rfc)
+    sfm = sfm.fit(X,y)
+
+    columns = X.columns.values
+    support = sfm.get_support()
+    return np.ndarray.tolist(columns[support])
 # Your solution code here

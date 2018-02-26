904d522b86c8a39a0dd811b220a8db5d78c08a10
cchopade <cchopade@gmail.com>
2018-02-26 03:55:00 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -6,6 +6,17 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
+def percentile_k_features(df,k = 20):
+    import numpy as np
+    X = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
 
+    select = SelectPercentile(f_regression, k)
+    X_new = select.fit_transform(X,y)
+
+    columns = X.columns.values
+    support = select.get_support()
+
+    return np.ndarray.tolist(columns[support])
 
 # Write your solution here:
--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -6,7 +6,19 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
 
+def rf_rfe(df):
+    import numpy as np
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
 
+    rfc = RandomForestClassifier()
+    nfeatures = df.shape[1]/2
+
+    rfe = RFE(rfc, n_features_to_select = nfeatures)
+    rfe = rfe.fit(X,y)
+
+    columns = X.columns.values
+    support = rfe.support_
+
+    return np.ndarray.tolist(columns[support])
 # Your solution code here
-
-
